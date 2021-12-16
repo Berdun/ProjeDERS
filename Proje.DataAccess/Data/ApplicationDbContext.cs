e2641909b534bf490130e017c80f24ebe1ff2124
Baha <Baha@DESKTOP-PD7J2CT>
2021-12-16 08:30:07 +0300

    Add project files.

---
+++ Proje.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Proje.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proje.DataAccess;
+public class ApplicationDbContext :IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categorys { get; set; }
+    public DbSet<Category> Categories {  get; set; }
+    public DbSet<CoverType> CoverTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetail { get; set; }
+}
---
+++ Proje.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Proje.DataAccess.Repository.IRepository;
+using Proje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
